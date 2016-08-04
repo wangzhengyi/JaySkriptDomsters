cd509db66bf42d8605c4ac2fd5afee446538073a
wangzhengyi <wzyll1314@gmail.com>
2016-08-04 23:34:34 +0800

    完成about.html的导航功能

--- scripts/global.js
+++ scripts/global.js
@@ -157,5 +157,47 @@ function prepareSlideShow() {
 }
 
 
+function showSection(id) {
+	var sections = document.getElementsByTagName("section");
+	for (var i = 0; i < sections.length; i ++) {
+		if (sections[i].getAttribute("id") != id) {
+			sections[i].style.display = "none";
+		} else {
+			sections[i].style.display = "block";
+		}
+	}
+}
+
+function prepareInternalnav() {
+	if (!document.getElementsByTagName || !document.getElementById) {
+		return false;
+	}
+
+	var articles = document.getElementsByTagName("article");
+	if (articles.length <= 0) {
+		return false;
+	}
+
+	var navs = articles[0].getElementsByTagName("nav");
+	if (navs.length == 0) {
+		return false;
+	}
+
+	var links = navs[0].getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		var hrefText = links[i].getAttribute("href").split("#")[1];
+		if (!document.getElementById(hrefText)) {
+			continue;
+		}
+
+		links[i].destination = hrefText;
+		links[i].onclick = function () {
+			showSection(this.destination);
+		};
+		document.getElementById(hrefText).style.display = "none";
+	}
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideShow);
+addLoadEvent(prepareInternalnav);
